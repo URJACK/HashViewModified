847f4b809fb2823feeb754bacc259d2504a4dc1d
URJACK <316585692@qq.com>
2020-03-04 12:41:38 -0800

    merge

--- routes/net_packets.rb
+++ routes/net_packets.rb
@@ -1,3 +1,20 @@
 get '/packets' do
+  @wordlists = Wordlists.all
+  @debugindex = params[:id]
   haml :packets_index
+end
+
+get '/debug' do
+  @wordlist = Wordlists.new
+  hash = rand(36**8).to_s(36)
+  wordlist = Wordlists.new
+  wordlist.type = 'dynamic'
+  wordlist.scope = 'hashfile'
+  wordlist.name = 'DYNAMIC [hashfile] - '
+  wordlist.path = 'control/wordlists/wordlist-' + hash + '.txt'
+  wordlist.size = 0
+  wordlist.checksum = nil
+  wordlist.lastupdated = Time.now
+  wordlist.save
+  redirect to("/home")
 end
